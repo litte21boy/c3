2122deb171cfe21733059b19af3bb3c97b143037
Your Name <you@example.com>
2019-01-16 10:14:08 +0800

    构建应用功能框架

--- main.c
+++ main.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    int code=0;
+    printf("guanliminganfuxitong\n");
+    while (1)
+    {
+        printf("1--tianjiaminganzifu\n");
+        printf("2--shanchuminganzifu\n");
+        printf("3--chakansuoyouminganzifu\n");
+        printf("4--tihuanminganzifu\n");
+        printf("5--tuichuchengxu\n");
+
+        printf("xuanzexiangyingdegongnengbianhao\n");
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            printf("tuichuxitong\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
